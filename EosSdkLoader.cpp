75f6a0304246b66184218e3656961960536ded95
Benoit Sigoure <tsuna@aristanetworks.com>
2013-11-15 02:00:16 -0800

    @1542758 Accept a --app argument that points to a .so to load.

--- EosSdkLoader.cpp
+++ EosSdkLoader.cpp
@@ -1,17 +1,42 @@
 // Copyright (c) 2013 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
 
+#include <cerrno>
+#include <cstring>
+#include <unistd.h>
+
+#include <iostream>
+
 #include <Agent/AgentBaseLib.h>
 
-int
-main( int argc, char **argv ) {
-   ExtraOption_t extraOpt = { "load", "", "SDK extension library to load" };
+
+static AgentBase::AgentContainer::Ptr
+createAgentContainer( int argc, char **argv ) {
+   ExtraOption_t extraOpt = { "app", "", "Path to the app to launch" };
 
    auto ac = agentContainerIs( "EosSdkAgentContainer", argc, argv, false, 1,
                                &extraOpt );
+   Tac::String app = ac->extraArg()->extraArg("app");
+   if ( app.empty() ) {
+      std::cerr << "Invalid usage: please specify --app=/path/to/libFoo.so"
+         << std::endl;
+      return 0;
+   }
+   if ( access( app.charPtr(), R_OK ) ) {
+      std::cerr << "Invalid --app path: " << strerror( errno ) << std::endl;
+      return 0;
+   }
+   return ac;
+}
 
-   ac->doAddAgent( "EosSdk", "EosSdk::Agent", "" );
-   ac->doRunAgents();
 
-   return 0;
+int
+main( int argc, char **argv ) {
+   auto container = createAgentContainer( argc, argv );
+   if (!container) {
+      return 1;
+   }
+   container->doAddAgent( "EosSdk", "EosSdk::Agent", "" );
+   container->doRunAgents();
+   return 42;  // We should never get back here.
 }
