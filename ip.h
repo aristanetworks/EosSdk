0a7fffe0e36875713e9a7d245499e744fcc7c05a
Benoit Sigoure <tsuna@aristanetworks.com>
2013-12-06 17:27:57 -0800

    @1575890 Minor indentation fixes.  No actual code change.

--- ip.h
+++ ip.h
@@ -19,88 +19,88 @@
 namespace eos {
 
 typedef enum {
-AF_NULL = 0,
-AF_IPV4 = 4,
-AF_IPV6 = 6
+   AF_NULL = 0,
+   AF_IPV4 = 4,
+   AF_IPV6 = 6,
 } af_t;
 
 // An IP address type that supports IPv4 and IPv6
 //
 class EOS_SDK_PUBLIC ip_addr_t {
-public:
-ip_addr_t();
-ip_addr_t(af_t, uint8_t const * ip_addr);  // copied; in network byte order
-explicit ip_addr_t(struct in_addr const &);
-explicit ip_addr_t(struct in6_addr const &);
-explicit ip_addr_t(char const *); // converts from IPv4/v6 string
-explicit ip_addr_t(uint32_be_t addr_v4);
-//= hidden
-// Conversion constructors for Arnet types
-explicit ip_addr_t(Arnet::IpAddr const &);
-explicit ip_addr_t(Arnet::Ip6Addr const &);
-//= end_hidden
+ public:
+   ip_addr_t();
+   ip_addr_t(af_t, uint8_t const * ip_addr);  // copied; in network byte order
+   explicit ip_addr_t(struct in_addr const &);
+   explicit ip_addr_t(struct in6_addr const &);
+   explicit ip_addr_t(char const *); // converts from IPv4/v6 string
+   explicit ip_addr_t(uint32_be_t addr_v4);
+   //= hidden
+   // Conversion constructors for Arnet types
+   explicit ip_addr_t(Arnet::IpAddr const &);
+   explicit ip_addr_t(Arnet::Ip6Addr const &);
+   //= end_hidden
 
-bool operator==(ip_addr_t const &other) const;
+   bool operator==(ip_addr_t const &other) const;
 
-af_t af() const;
-uint8_t const * addr() const;
-uint32_be_t addr_v4() const;
+   af_t af() const;
+   uint8_t const * addr() const;
+   uint32_be_t addr_v4() const;
 
-std::string to_string() const;
+   std::string to_string() const;
 
-//=> private:
-union {
-   uint8_t bytes[16];
-   uint32_t words[4];
-} addr_;
-uint8_t af_;   // af_t in one byte.
+   //=> private:
+   union {
+      uint8_t bytes[16];
+      uint32_t words[4];
+   } addr_;
+   uint8_t af_;   // af_t in one byte.
 };
 
 // A v4 or v6 IP route prefix of a network address and a prefix_length.
 // Network adresses have non-zero bits in only the first prefix_length
 // bits (in big/network endian)
 class EOS_SDK_PUBLIC ip_prefix_t {
-public:
-ip_prefix_t();
-ip_prefix_t(ip_addr_t const &, int);
-ip_prefix_t(char const *);  // Converts IPv4/IPv6 prefix string
+ public:
+   ip_prefix_t();
+   ip_prefix_t(ip_addr_t const &, int);
+   ip_prefix_t(char const *);  // Converts IPv4/IPv6 prefix string
 
-af_t af() const;
-void const * network();
-uint32_t network_v4() const;
-int prefix_length() const;
+   af_t af() const;
+   void const * network();
+   uint32_t network_v4() const;
+   int prefix_length() const;
 
-std::string to_string() const;  // returns "10.2.3.4/24", "::1" or "::1/128", etc
+   std::string to_string() const;  // returns "10.2.3.4/24", "::1" or "::1/128", etc
 
-//=> private:
-ip_addr_t addr_;
-uint8_t prefix_length_;
+   //=> private:
+   ip_addr_t addr_;
+   uint8_t prefix_length_;
 };
 
 // An IPv4 or IPv6 address with a full 32-bit mask, to allow for non-
 // contiguous subnet masks (e.g., for ACLs).
 class EOS_SDK_PUBLIC ip_addr_mask_t {
-public:
-ip_addr_mask_t();
-ip_addr_mask_t(ip_addr_t const &, int mask_length);
+ public:
+   ip_addr_mask_t();
+   ip_addr_mask_t(ip_addr_t const &, int mask_length);
 
-af_t af() const;
-uint8_t const * addr() const;
-uint32_be_t addr_v4() const;
-int mask_length() const; // e.g., 24
-uint32_be_t mask() const; // e.g., 0xffffff00
+   af_t af() const;
+   uint8_t const * addr() const;
+   uint32_be_t addr_v4() const;
+   int mask_length() const; // e.g., 24
+   uint32_be_t mask() const; // e.g., 0xffffff00
 
-std::string to_string() const;
+   std::string to_string() const;
 
-//=> private:
-ip_addr_t addr_;
-uint8_t mask_length_;
+   //=> private:
+   ip_addr_t addr_;
+   uint8_t mask_length_;
 
-//= hidden
-// Private helper function used by EOS SDK users of ip.h
-Arnet::IpAddrWithFullMask getIpAddrWithFullMask() const;
-Arnet::Ip6AddrWithMask getIp6AddrWithMask() const;
-//= end_hidden
+   //= hidden
+   // Private helper function used by EOS SDK users of ip.h
+   Arnet::IpAddrWithFullMask getIpAddrWithFullMask() const;
+   Arnet::Ip6AddrWithMask getIp6AddrWithMask() const;
+   //= end_hidden
 };
 
 // e.g., "10.1.2.0/24" or "fffe::0001/96"
