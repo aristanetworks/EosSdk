8e3d1130ecdf721b3fcbca691b398230bdce1248
Benoit Sigoure <tsuna@aristanetworks.com>
2013-12-31 02:08:58 -0800

    @1605276 Add missing stubs for the new event_loop APIs.

---
+++ event_loop.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2013 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+#include "eos/panic.h"
+#include "eos/event_loop.h"
+
+namespace eos {
+
+event_loop::event_loop() {
+}
+
+event_loop::~event_loop() {
+}
+
+void
+event_loop::run(seconds_t duration) const {
+   if(duration < 0){
+      panic("duration must be 0 or greater");
+   }
+   // TODO: Implement.
+}
+
+}
