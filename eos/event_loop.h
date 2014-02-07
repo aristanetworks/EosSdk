9f58ba3d5be1d41087e1f2f31f01f0a09e6e432a
Andre Pech <apech@aristanetworks.com>
2014-02-07 15:31:09 -0800

    @1656067 Improve EosSdk script support

--- eos/event_loop.h
+++ eos/event_loop.h
@@ -13,6 +13,11 @@ class EOS_SDK_PUBLIC event_loop {
    event_loop();
    ~event_loop();
 
+   // Wait for all managers to be initialized. Generally called right
+   // after creating the event_loop, as an alternative to creating an
+   // agent_handler and then waiting for on_initialized to be called.
+   void wait_for_initialized() const;
+   
    // Run the event loop for `duration` seconds. If no duration is
    // specified, all pending events will be executed and control will
    // be returned to the caller.
