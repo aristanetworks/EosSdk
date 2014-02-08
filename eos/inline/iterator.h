2f483167457aa309c32a25d95decdbff4279e080
Benoit Sigoure <tsuna@aristanetworks.com>
2014-02-08 02:23:42 -0800

    @1656594 Add a postfix operator++ to our iterators.

--- eos/inline/iterator.h
+++ eos/inline/iterator.h
@@ -7,6 +7,14 @@
 namespace eos {
 
 template <typename T, typename Impl>
+inline iter_base<T, Impl>
+iter_base<T, Impl>::operator++(int) {
+   iter_base<T, Impl> me = *this;
+   ++(*this);
+   return me;
+}
+
+template <typename T, typename Impl>
 inline bool
 iter_base<T, Impl>::operator!=(iter_base<T, Impl> const & rhs) const {
    return !(*this == rhs);
