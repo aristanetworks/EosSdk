ff1221eba02b1457a09de55d278cc430f35a87c5
Benoit Sigoure <tsuna@aristanetworks.com>
2014-02-21 17:13:04 -0800

    @1675045 Ship the Python bindings and allow building them conditionally.

--- build.sh
+++ build.sh
@@ -7,7 +7,26 @@ export CFLAGS
 CXXFLAGS=$CFLAGS
 export CXXFLAGS
 
+# Extract the arguments that we need to forward to `./configure'.
+# Other arguments will be passed to `make'.  This is so that one can
+# do something along the lines of `./build.sh --enable-python check',
+# for instance.
+configure_flags=''
+for arg; do
+   case $arg in
+      (--enable-*|--disable-*|--with-*|--without-*)
+         configure_flags="$configure_flags $arg"
+         shift
+         ;;
+   esac
+done
+
 set -e
 test -f configure || ./bootstrap
-test -f Makefile || ./configure --build=i686-pc-linux-gnu --host=i686-pc-linux-gnu --program-prefix= --disable-dependency-tracking --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include --libdir=/usr/lib --libexecdir=/usr/libexec --localstatedir=/var --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info
+test -f Makefile || ./configure $configure_flags \
+   --build=i686-pc-linux-gnu --host=i686-pc-linux-gnu --program-prefix= \
+   --prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin \
+   --sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include \
+   --libdir=/usr/lib --libexecdir=/usr/libexec --localstatedir=/var \
+   --sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info
 exec make "$@"
