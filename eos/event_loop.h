a22113e2ff72f02007bc081b38b15a20f8648e6d
Andre Pech <apech@aristanetworks.com>
2014-03-04 08:56:47 -0800

    @1689227 Doxygen-style comments for eos/event_loop.h

--- eos/event_loop.h
+++ eos/event_loop.h
@@ -8,19 +8,26 @@
 
 namespace eos {
 
+/**
+ * The EOS event loop.
+ */
 class EOS_SDK_PUBLIC event_loop {
  public:
    event_loop();
    ~event_loop();
 
-   // Wait for all managers to be initialized. Generally called right
-   // after creating the event_loop, as an alternative to creating an
-   // agent_handler and then waiting for on_initialized to be called.
+   /**
+    * Wait for all managers to be initialized. Generally called right
+    * after creating the event_loop, as an alternative to creating an
+    * agent_handler and then waiting for on_initialized to be called.
+    */
    void wait_for_initialized() const;
 
-   // Run the event loop for `duration` seconds. If no duration is
-   // specified, all pending events will be executed and control will
-   // be returned to the caller.
+   /**
+    * Run the event loop for `duration` seconds. If no duration is
+    * specified, all pending events will be executed and control will
+    * be returned to the caller.
+    */
    void run(seconds_t duration=0) const;
 
  private:
