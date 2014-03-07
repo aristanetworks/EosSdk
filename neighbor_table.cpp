15987fa77ad06d9fc5ba99d915aeec1f74d8c02c
Benoit Sigoure <tsuna@aristanetworks.com>
2014-03-07 12:43:43 -0800

    @1694653 Fix stubs for ARP table.

--- neighbor_table.cpp
+++ neighbor_table.cpp
@@ -1,17 +1,19 @@
 // Copyright (c) 2014 Arista Networks, Inc.  All rights reserved.
 // Arista Networks, Inc. Confidential and Proprietary.
 
+#include "eos/neighbor_table.h"
+
 namespace eos {
 
 class neighbor_table_mgr_impl : public neighbor_table_mgr {
  public:
-   intf_mgr_impl() {
+   neighbor_table_mgr_impl() {
    }
 };
 
 neighbor_table_mgr *
 get_neighbor_table_mgr() {
-   static intf_mgr_impl impl;
+   static neighbor_table_mgr_impl impl;
    return &impl;
 }
 
@@ -19,7 +21,7 @@ neighbor_table_mgr::neighbor_table_mgr() {
 }
 
 neighbor_entry_t
-neighbor_table_mgr::neighbor_entry(intf_id_t intf, ip_addr_t addr) {
+neighbor_table_mgr::neighbor_entry(intf_id_t intf, ip_addr_t addr) const {
    return neighbor_entry_t();
 }
 
