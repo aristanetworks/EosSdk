694b785ca347cb7036e5fc7d3334f0dff561788f
Andrew Fort <afort@aristanetworks.com>
2014-03-06 17:15:06 -0800

    @1693433 Doxygenate ip_route

--- eos/ip_route.h
+++ eos/ip_route.h
@@ -14,6 +14,9 @@ typedef uint32_t ip_route_tag_t;
 typedef uint32_t ip_route_metric_t;
 typedef uint8_t ip_route_preference_t;
 
+/**
+ * An IP route key, consisting of a prefix and preference.
+ */
 class EOS_SDK_PUBLIC ip_route_key_t {
  public:
    ip_route_key_t();
@@ -25,28 +28,40 @@ class EOS_SDK_PUBLIC ip_route_key_t {
    ip_route_metric_t metric;
 };
 
-// An IP v4/v6 static route.
-// Nexthop "via" for this route are stored separately
+/**
+ * An IP v4/v6 static route.
+ * "Via", or nexthops, for this route are stored separately and are
+ * associated by route key.
+ */
 class EOS_SDK_PUBLIC ip_route_t {
  public:
    ip_route_t();
+   /// Creates an IP static route for the route key
    explicit ip_route_t(ip_route_key_t const & route_key);
 
-   ip_route_key_t key;
+   ip_route_key_t key;  ///< The route's key
 
-   ip_route_tag_t tag;       // A numbered tag, useful for classifying routes
-   bool persistent;          // If true, the route is stored to startup-config
+   ip_route_tag_t tag;  ///< A numbered tag, used for table segregation
+   bool persistent;     ///< If true, the route is stored in the startup-config
 };
 
-// A via entry, describing nexthop information
+/**
+ * A Via describing a particular set of nexthop information.
+ *
+ * A Via can describe either a nexthop IP address (hop), interface
+ * (intf) or nexthop group name to either forward traffic to, or
+ * drop traffic if the interface is Null0.
+ */
 class EOS_SDK_PUBLIC ip_route_via_t {
  public:
    ip_route_via_t();
+   /// Creates a route via for a route key
    explicit ip_route_via_t(ip_route_key_t const & route_key);
 
-   ip_route_key_t route_key; // Key for the route this via is attached to
-   ip_addr_t hop;            // IP v4/v6 nexthop address
-   intf_id_t intf;           // Use the named interface if not a default intf_id_t
+   ip_route_key_t route_key; ///< Key for the route this via is attached to
+   ip_addr_t hop;            ///< IP v4/v6 nexthop address
+   intf_id_t intf;           ///< Use the named interface if not a default intf_id_t
+
    // using intf Null0 installs a 'drop' route for the given prefix and preference
 
    /**
@@ -63,6 +78,7 @@ class EOS_SDK_PUBLIC ip_route_via_t {
 
 class ip_route_iter_impl;
 
+/// An IP route iterator
 class EOS_SDK_PUBLIC ip_route_iter_t : public iter_base<ip_route_t,
                                                         ip_route_iter_impl> {
  private:
@@ -73,6 +89,7 @@ class EOS_SDK_PUBLIC ip_route_iter_t : public iter_base<ip_route_t,
 
 class ip_route_via_iter_impl;
 
+/// A route via iterator
 class EOS_SDK_PUBLIC ip_route_via_iter_t : public iter_base<ip_route_via_t,
                                                             ip_route_via_iter_impl> {
  private:
@@ -81,71 +98,92 @@ class EOS_SDK_PUBLIC ip_route_via_iter_t : public iter_base<ip_route_via_t,
 };
 
 
-// Manages IP static route configuration
+/// The manager for IP static route configuration
 class EOS_SDK_PUBLIC ip_route_mgr {
  public:
-   // Set the manager to only interact with routes with the given
-   // tag. If this tag is set to a non-zero number, then methods on
-   // this class will only affect or expose routes with the given tag.
+   /**
+    * Set the manager to only interact with routes with the given tag.
+    *
+    * If this tag is set to a non-zero number, then methods on this
+    * class will only affect or expose routes with the given tag.
+    */
    void tag_is(uint32_t);
-   // Returns the current tag, or 0 if no tag is set.
+
+   /// Returns the current tag, or 0 if no tag is set.
    uint32_t tag() const;
 
-   // Resync provides a mechanism to set the static routing table to a
-   // known state. To start a resync, call resync_init() and then use
-   // ip_route_set() and ip_route_via_set() to populate the static
-   // routing table with the known entries. After all entries have
-   // been set, call resync_complete(), which will delete all existing
-   // routes and vias that were not added or modified during resync.
-   // If a tag is set, resync will only delete routes and vias
-   // corresponding to the current tag.
-   // During resync, this manager will act like it is referencing a
-   // temporary table that starts off empty. Thus, methods like exist,
-   // _del, and getters will act only on that temporary table,
-   // regardless of the real values in Sysdb. The one exception is
-   // iteration; they will traverse the table stored in Sysdb,
-   // regardless of whether or not the manager is in resync mode.
+   /**
+    * Resync provides a mechanism to set routes to a known state.
+    *
+    * To start a resync, call resync_init() and then use
+    * ip_route_set() and ip_route_via_set() to populate the static
+    * routing table with the known entries. After all entries have
+    * been set, call resync_complete(), which will delete all existing
+    * routes and vias that were not added or modified during resync.
+    * If a tag is set, resync will only delete routes and vias
+    * corresponding to the current tag.
+    *
+    * During resync, this manager will act like it is referencing a
+    * temporary table that starts off empty. Thus, methods like exist,
+    * del, and getters will act only on that temporary table,
+    * regardless of the real values in Sysdb. The one exception is
+    * iteration; they will traverse the table stored in Sysdb,
+    * regardless of whether or not the manager is in resync mode.
+    */
    void resync_init();
+   /// Completes any underway resync operation.
    void resync_complete();
 
    typedef bool (*callback_func_route)(ip_route_t const &, void * context);
    typedef bool (*callback_func_via)(ip_route_via_t const &, void * context);
 
-   // Iterate across all configured static routes. If a tag is set,
-   // only return routes that have match the current tag.
+   /**
+    * Iterates across all configured static routes.
+    * If a tag is set, only return routes that have match the current tag.
+    */
    ip_route_iter_t ip_route_iter() const;
    void ip_route_foreach(callback_func_route handler, void * context);
    void ip_route_foreach(callback_func_route handler, void * context,
                          ip_route_t const & bookmark);
 
-   // Iterate across configured nexthops for a given route key, i.e.,
-   // emit all ip_route_via_t's for a given route key. If a tag is
-   // set, only return vias on routes that match the current tag.
+   /**
+    * Iterates across configured nexthops for a given route key,
+    * i.e., emit all ip_route_via_t's for a given route key. If a tag
+    * is set, only return vias on routes that match the current tag.
+    */
    ip_route_via_iter_t ip_route_via_iter(ip_route_key_t const &) const;
    void ip_route_via_foreach(ip_route_key_t const &,
                              callback_func_via handler, void * context);
 
-   // Tests for existence of any routes matching the route key in the
-   // switch config. If a tag is set and the route belongs to a
-   // different tag, this function returns false.
+   /**
+    * Tests for existence of any routes matching the route key in the config.
+    * If a tag is set and the route belongs to a different tag, this
+    * function returns false.
+    */
    bool exists(ip_route_key_t const &) const;
-   // Tests if the given via exists.
+   /// Tests if the given via exists.
    bool exists(ip_route_via_t const &) const;
 
    // Route management functions
 
-   // Gets a static route, or panics if the route key does not exist.
+   /// Gets a static route, or panics if the route key does not exist.
    ip_route_t ip_route(ip_route_key_t const &);
-   // Inserts or updates a static route into the switch configuration.
+   /// Inserts or updates a static route into the switch configuration.
    void ip_route_set(ip_route_t const &);
-   // Removes all ECMP vias matching the route key, and the route itself.
+   /// Removes all ECMP vias matching the route key, and the route itself.
    void ip_route_del(ip_route_key_t const &);
 
-   // Adds a via to an ip_route_t. Will panic if the corresponding route
-   // does not match the currently configured tag.
+   /**
+    * Adds a via to an ip_route_t.
+    * Will call panic() if the corresponding route does not match the
+    * currently configured tag.
+    */
    void ip_route_via_set(ip_route_via_t const &);
-   // Removes a via from an ip_route_t. When all vias are removed,
-   // the route still exists with no nexthop information.
+   /**
+    * Removes a via from an ip_route_t.
+    * When all vias are removed, the route still exists with no
+    * nexthop information.
+    */
    void ip_route_via_del(ip_route_via_t const &);
 
  protected:
@@ -154,6 +192,7 @@ class EOS_SDK_PUBLIC ip_route_mgr {
    EOS_SDK_DISALLOW_COPY_CTOR(ip_route_mgr);
 };
 
+/// Returns the IP route manager
 ip_route_mgr * get_ip_route_mgr() EOS_SDK_PUBLIC;
 
 }  // end namespace eos
