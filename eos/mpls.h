e77d1e92231fc97779af182ee732b47e739a9758
Andre Pech <apech@aristanetworks.com>
2014-05-17 11:08:26 -0700

    @1823393 Swig mpls.h and mpls_route.h.

--- eos/mpls.h
+++ eos/mpls.h
@@ -70,15 +70,15 @@ class EOS_SDK_PUBLIC mpls_label_t {
  * MPLS reserved label definitions (0..15).
  * See http://www.iana.org/assignments/mpls-label-values/mpls-label-values.xhtml
  */
-static mpls_label_t const mpls_label_explicit_null_v4(0);
-static mpls_label_t const mpls_label_router_alert(1);
-static mpls_label_t const mpls_label_explicit_null_v6(2);
-static mpls_label_t const mpls_label_implicit_null(3);
+static mpls_label_t const mpls_label_explicit_null_v4 = 0;
+static mpls_label_t const mpls_label_router_alert = 1;
+static mpls_label_t const mpls_label_explicit_null_v6 = 2;
+static mpls_label_t const mpls_label_implicit_null = 3;
 // 4..6 unassigned
-static mpls_label_t const mpls_label_entropy_indicator(7);
+static mpls_label_t const mpls_label_entropy_indicator = 7;
 // 8..12 unassigned
-static mpls_label_t const mpls_label_gal(13);
-static mpls_label_t const mpls_label_oam_alert(14);
+static mpls_label_t const mpls_label_gal = 13;
+static mpls_label_t const mpls_label_oam_alert = 14;
 // 15 unassigned
 
 }  // end namespace eos
