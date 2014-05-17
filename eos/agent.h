aeb9d4c8660902a735d62a6cec976b431fa99cbb
Andre Pech <apech@aristanetworks.com>
2014-05-17 13:54:41 -0700

    @1823546 Initial header changes for EOS SDK agent runnability and option configuration.

--- eos/agent.h
+++ eos/agent.h
@@ -4,6 +4,8 @@
 #ifndef EOS_AGENT_H
 #define EOS_AGENT_H
 
+#include <string>
+
 #include <eos/base.h>
 #include <eos/base_handler.h>
 #include <eos/base_mgr.h>
@@ -34,6 +36,23 @@ class EOS_SDK_PUBLIC agent_handler : public base_handler<agent_mgr, agent_handle
     * event.
     */
    virtual void on_initialized();
+
+   /**
+    * Handler called when the agent has been enabled/disabled.
+    *
+    * The default implementation will synchronously call the
+    * agent_mgr's agent_shutdown_complete method. If you override
+    * on_agent_enabled, then you are also responsible for calling
+    * agent_shutdown_complete.
+    * 
+    */
+   virtual void on_agent_enabled(bool enabled);
+
+   /**
+    * Handler called when the configuration options of the agent
+    * have changed.
+    */
+   virtual void on_agent_option(std::string const & name, std::string const & value);
 };
 
 class EOS_SDK_PUBLIC agent_mgr : public base_mgr<agent_handler> {
@@ -55,6 +74,40 @@ class EOS_SDK_PUBLIC agent_mgr : public base_mgr<agent_handler> {
      * though not across reboots.
      */
     static uint32_t id(const char * agent_name);
+
+    /**
+     * Return whether the agent is enabled or not.
+     *
+     * When enabled is false, the agent is responsible for cleaning
+     * itself up as necessary and then calling running_is(false).
+     */
+    virtual bool enabled() const = 0;
+
+    /**
+     * Get the configured value for the given agent option.
+     *
+     * If no value has been set for the option, the empty string is
+     * returned.
+     */
+    virtual std::string agent_option(std::string const & name) const = 0;
+
+    /**
+     * Called when agent graceful shutdown has successfully completed.
+     *
+     * If the agent requires special handling to cleanup state when
+     * the agent is disabled, then an agent_handler must be created
+     * that overrides the on_agent_enabled method and appropriately
+     * cleans up when the agent is disabled. Once the agent is
+     * ready to be killed, it must call agent_shutdown_complete,
+     * after which it will be killed by ProcMgr.
+     * 
+     * This method is called automatically by the default implementation
+     * of on_agent_enabled when the agent is disabled. If no special
+     * cleanup handling is required, then simply do not override
+     * on_agent_enabled and the agent will be killed by ProcMgr as
+     * soon as it is administratively disabled.
+     */
+    virtual void agent_shutdown_complete() = 0;
 
  protected:
     agent_mgr() EOS_SDK_PRIVATE;
