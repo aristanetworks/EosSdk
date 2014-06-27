93c46eb96f1cee307071e913adeb4b445817eeed
Ryan Madsen <ryan@aristanetworks.com>
2014-06-27 13:49:17 -0700

    @1910730 Bug in policy map's stubbed out iterator definition.

--- iterator.cpp
+++ iterator.cpp
@@ -79,6 +79,6 @@ template class iter_base<nexthop_group_t, nexthop_group_iter_impl>;
 template class iter_base<flow_entry_t, flow_entry_iter_impl>;
 template class iter_base<mpls_route_t, mpls_route_iter_impl>;
 template class iter_base<mpls_route_via_t, mpls_route_via_iter_impl>;
-template class iter_base<policy_map_t, policy_map_iter_impl>;
+template class iter_base<policy_map_key_t, policy_map_iter_impl>;
 
 }
