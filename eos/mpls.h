a360d442088a77e4a812af9cb781b85eb6552414
Benoit Sigoure <tsuna@arista.com>
2014-07-03 10:25:31 -0700

    @1925072 Add a setter/getter for the label of an mpls_label_t.

--- eos/mpls.h
+++ eos/mpls.h
@@ -50,7 +50,10 @@ enum mpls_payload_type_t {
 class EOS_SDK_PUBLIC mpls_label_t {
  public:
    mpls_label_t();
-   mpls_label_t(uint32_t label);
+   explicit mpls_label_t(uint32_t label);
+
+   uint32_t label() const;
+   void label_is(uint32_t label);
 
    /**
     * Only the "default mpls label" returned by the default
@@ -60,7 +63,12 @@ class EOS_SDK_PUBLIC mpls_label_t {
    bool operator==(mpls_label_t const & other) const;
    bool operator!=(mpls_label_t const & other) const;
    bool operator<(mpls_label_t const & other) const;
-   
+
+   enum {
+      MIN = 0,
+      MAX = 0xFFFFF,  // 1048575
+   };
+
  private:
    friend struct MplsLabelHelper;
    uint32_t label_;
@@ -70,15 +78,15 @@ class EOS_SDK_PUBLIC mpls_label_t {
  * MPLS reserved label definitions (0..15).
  * See http://www.iana.org/assignments/mpls-label-values/mpls-label-values.xhtml
  */
-static mpls_label_t const mpls_label_explicit_null_v4 = 0;
-static mpls_label_t const mpls_label_router_alert = 1;
-static mpls_label_t const mpls_label_explicit_null_v6 = 2;
-static mpls_label_t const mpls_label_implicit_null = 3;
+static mpls_label_t const mpls_label_explicit_null_v4 = mpls_label_t(0);
+static mpls_label_t const mpls_label_router_alert = mpls_label_t(1);
+static mpls_label_t const mpls_label_explicit_null_v6 = mpls_label_t(2);
+static mpls_label_t const mpls_label_implicit_null = mpls_label_t(3);
 // 4..6 unassigned
-static mpls_label_t const mpls_label_entropy_indicator = 7;
+static mpls_label_t const mpls_label_entropy_indicator = mpls_label_t(7);
 // 8..12 unassigned
-static mpls_label_t const mpls_label_gal = 13;
-static mpls_label_t const mpls_label_oam_alert = 14;
+static mpls_label_t const mpls_label_gal = mpls_label_t(13);
+static mpls_label_t const mpls_label_oam_alert = mpls_label_t(14);
 // 15 unassigned
 
 }  // end namespace eos
