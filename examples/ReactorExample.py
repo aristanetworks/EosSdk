32fc5345407757ef8b9c9f8d272bdd67dce75835
Ryan Madsen <ryan@aristanetworks.com>
2014-07-20 12:29:00 -0700

    @1960412 ReactorExample fixes.

--- examples/ReactorExample.py
+++ examples/ReactorExample.py
@@ -8,8 +8,9 @@ class MyIntfHandler( eossdk.AgentHandler, eossdk.IntfHandler ):
    def __init__( self, intfMgr, agentMgr ):
       eossdk.AgentHandler.__init__( self, agentMgr )
       eossdk.IntfHandler.__init__( self, intfMgr )  # pylint: disable-msg=W0233
-      self.numIntfChanges = 0
-      print "Initialized"
+      self.intfMgr_ = intfMgr
+      self.numIntfChanges_ = 0
+      print "Constructed"
 
    # Callback provided by AgentHandler when all state is synchronized
    def on_initialized( self ):
@@ -17,11 +18,14 @@ class MyIntfHandler( eossdk.AgentHandler, eossdk.IntfHandler ):
       self.watch_all_intfs( True )  # pylint: disable-msg=E1101
 
    # Callback provided by IntfHandler when an interface's configuration changes
-   def on_admin_enabled( self, intfId, operState ):
+   def on_oper_status( self, intfId, operState ):
       intfState = 'up' if operState == eossdk.INTF_OPER_UP else 'down'
       print "The state of", intfId.to_string(), "is now", intfState
       self.numIntfChanges += 1
-      print "   We have seen", self.numIntfChanges, "state changes"
+      print "   We have seen", self.numIntfChanges_, "state changes"
+      self.intfMgr_.description_is(intfId,
+                                   "Changed state [%d]" % self.numIntfChanges_)
+      
 
 sdk = eossdk.Sdk()
 mta = MyIntfHandler( sdk.get_intf_mgr(), sdk.get_agent_mgr() )
