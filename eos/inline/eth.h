2ad62d18e4b7fbe14eced779fb6cf239d9d569e9
Benoit Sigoure <tsuna@arista.com>
2014-07-22 13:10:43 -0700

    @1964761 Do not use parentheses for non-class list-initializers.

--- eos/inline/eth.h
+++ eos/inline/eth.h
@@ -11,11 +11,11 @@ inline eth_addr_t::eth_addr_t() : bytes_() {
 
 inline eth_addr_t::eth_addr_t(uint8_t byte0, uint8_t byte1, uint8_t byte2,
                               uint8_t byte3, uint8_t byte4, uint8_t byte5)
-   : bytes_({ byte0, byte1, byte2, byte3, byte4, byte5 }) {
+   : bytes_{ byte0, byte1, byte2, byte3, byte4, byte5 } {
 }
 
 inline eth_addr_t::eth_addr_t(uint8_t const bytes[6])
-   : bytes_({ bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5] }) {
+   : bytes_{ bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5] } {
 }
 
 inline bool
