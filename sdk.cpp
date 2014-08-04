925ebbefd3032adcd774589c0c89c14fa29a7f63
Kevin Liu <liusijia@arista.com>
2014-08-04 12:23:12 -0700

    @1992562 trying to assign a name to sdk.

--- sdk.cpp
+++ sdk.cpp
@@ -35,6 +35,35 @@ sdk::sdk()
    impl.register_sdk(this);
 }
 
+sdk::sdk(std::string const name)
+   : acl_mgr_(0),
+     agent_mgr_(0),
+     aresolve_mgr_(0),
+     class_map_mgr_(0),
+     decap_group_mgr_(0),
+     directflow_mgr_(0),
+     eth_intf_mgr_(0),
+     eth_phy_intf_mgr_(0),
+     eth_lag_intf_mgr_(0),
+     event_loop_(0),
+     fib_mgr_(0),
+     intf_mgr_(0),
+     intf_counter_mgr_(0),
+     ip_intf_mgr_(0),
+     ip_route_mgr_(0),
+     mac_table_mgr_(0),
+     mlag_mgr_(0),
+     mount_mgr_(0),
+     mpls_route_mgr_(0),
+     neighbor_table_mgr_(0),
+     nexthop_group_mgr_(0),
+     policy_map_mgr_(0),
+     system_mgr_(0),
+     timeout_mgr_(0) {
+   name_ = name;
+   impl.register_sdk(this);
+}
+
 void delete_agent_mgr(agent_mgr *);
 
 sdk::~sdk() {
