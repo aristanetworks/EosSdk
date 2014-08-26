9de7a8d51c2a2070be61859fa4d28137ed8e18d9
Benoit Zanotti <jicks@aristanetworks.com>
2014-08-25 18:00:16 -0700

    @2034266 Fix ip_intf issue where the removal of an IP interface status did not cause

---
+++ intf_types.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2014 Arista Networks, Inc.  All rights reserved.
+// Arista Networks, Inc. Confidential and Proprietary.
+
+#include "eos/intf.h"
+
+namespace eos {
+
+intf_id_t::intf_id_t() {
+   
+}
+
+intf_id_t::intf_id_t(char const * name) :
+      intfId_() {
+   
+}
+
+intf_id_t::intf_id_t(std::string const & name) :
+      intfId_() {
+   
+}
+
+bool
+intf_id_t::is_null0() const {
+   return false;
+}
+
+intf_type_t
+intf_id_t::intf_type() const {
+   return INTF_TYPE_NULL;
+}
+
+std::string
+intf_id_t::to_string() const {
+   return NULL;
+}
+
+bool
+intf_id_t::operator!() const {
+   return false;
+}
+
+
+}
