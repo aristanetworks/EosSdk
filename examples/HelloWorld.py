6b0585f41e343a2d63cf885da70ee4ba5115cc29
Ryan Madsen <ryan@aristanetworks.com>
2014-09-04 15:50:00 -0700

    @2051994 Remove some spaces around parens.

--- examples/HelloWorld.py
+++ examples/HelloWorld.py
@@ -8,14 +8,14 @@ import sys
 # This is the python version of the HelloWorld.cpp program. See the
 # explanation there, or a more detailed walkthrough at the GitHub wiki.
 
-class HelloWorldAgent( eossdk.AgentHandler ):
-   def __init__( self, sdk ):
+class HelloWorldAgent(eossdk.AgentHandler):
+   def __init__(self, sdk):
       self.agentMgr = sdk.get_agent_mgr()
       self.tracer = eossdk.Tracer("HelloWorldPythonAgent")
       eossdk.AgentHandler.__init__(self, self.agentMgr)
       self.tracer.trace0("Python agent constructed")
 
-   def on_initialized( self ):
+   def on_initialized(self):
       self.tracer.trace0("Initialized")
       name = self.agentMgr.agent_option("name")
       if not name:
@@ -33,7 +33,7 @@ class HelloWorldAgent( eossdk.AgentHandler ):
          else:
             # Time for some social networking!
             self.tracer.trace3("Saying hi to %s", value)
-            self.agentMgr.status_set("greeting", "Hello %s!" % value )
+            self.agentMgr.status_set("greeting", "Hello %s!" % value)
    
    def on_agent_enabled(self, enabled):
       if not enabled:
