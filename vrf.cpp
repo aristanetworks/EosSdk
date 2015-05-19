d1cc5e064958e7a837eb45a49a6289ebb9871467
Yong Chen <chen@arista.com>
2015-05-19 10:23:30 -0700

    @2432404 Fix 2 test failures:

--- vrf.cpp
+++ vrf.cpp
@@ -44,6 +44,10 @@ DEFINE_STUB_MGR_CTOR(vrf_mgr)
 vrf_handler::vrf_handler(vrf_mgr *mgr) : base_handler(mgr) {
 }
 
+void vrf_handler::watch_all_vrfs(bool) {}
+
+void vrf_handler::watch_vrf(std::string const & vrf_name, bool) {}
+
 void vrf_handler::on_vrf_state(std::string vrf_name, vrf_state_t vrf_state) {
 }
 
