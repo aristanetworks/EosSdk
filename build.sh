2adcf4645613dcb92320cc8d2a2ecbcd2c848319
Avinash Kumar <avinash@arista.com>
2016-09-15 16:27:58 -0700

    @3529534 make this branch the new base

--- build.sh
+++ build.sh
@@ -48,9 +48,5 @@ set -e
 test -f configure || ./bootstrap
 test -f Makefile || ./configure $configure_flags \
    $configure_flags --program-prefix= \
-   #--prefix=/usr --exec-prefix=/usr --bindir=/usr/bin --sbindir=/usr/sbin \
-   #--sysconfdir=/etc --datadir=/usr/share --includedir=/usr/include \
-   #--libdir=/usr/lib --libexecdir=/usr/libexec --localstatedir=/var \
-   #--sharedstatedir=/var/lib --mandir=/usr/share/man --infodir=/usr/share/info
    --prefix=$sysroot/usr
 exec make "$@"
